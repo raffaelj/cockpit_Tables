b8ba1029e03a036527c3747ca117b7e06c4c7a5e
Raffael <github@raffaeljesche.de>
2019-03-25 19:15:39 +0100

    moved all addon files to root

--- bootstrap.php
+++ bootstrap.php
@@ -1,20 +1,57 @@
 <?php
 
-/*
- * This is a dummy bootstrap file to prevent a
- * `Fatal error: require(): Failed opening required ...`
- * when `\Lime\App` tries to load the module without an
- * existing `bootstrap.php`.
- *
- * Don't copy the whole repository into your addons folder.
- * You only need the one, that is named like this addon.
- *
- * Sorry for the confusion, but I like to keep some data
- * (e. g. .gitignore or a docs folder) in the root, that
- * aren't necessary in production use of the addon.
- *
- */
+// simple logging for debugging
+function debug($message) {
 
-$app->on('app.layout.contentbefore', function(){
-    echo '<p class="uk-panel"><span class="uk-badge uk-badge-warning"><i class="uk-margin-small-right uk-icon-warning"></i>' . basename(__DIR__) . '</span> You copied the wrong addon folder. Have a look at the addon\'s readme file for instructions.</p>';
+    global $cockpit;
+
+    if (!is_string($message) || !is_numeric($message))
+        $message = json_encode($message);
+
+    $time = date('Y-m-d H:i:s', time());
+
+    $cockpit('fs')->write("#storage:tmp/.log.txt", "$time - $message\r\n", FILE_APPEND);
+
+}
+
+// load database config
+
+$db_config = $app->retrieve('tables/db', []);
+
+$db_config = array_merge([
+      'host' => 'localhost'
+    , 'database' => ''
+    , 'user' => 'root'
+    , 'password' => ''
+    , 'prefix' => ''
+    , 'charset' => 'utf8'
+    ], $db_config
+);
+
+define('COCKPIT_TABLES_DB_HOST', $db_config['host']);
+define('COCKPIT_TABLES_DB_NAME', $db_config['database']);
+define('COCKPIT_TABLES_DB_CHAR', $db_config['charset']);
+define('COCKPIT_TABLES_DB_USER', $db_config['user']);
+define('COCKPIT_TABLES_DB_PASS', $db_config['password']);
+define('COCKPIT_TABLES_DB_PREF', $db_config['prefix']);
+    
+$app->on('admin.init', function() {
+
+    try {
+        $this->helpers['db'] = new \Tables\Helpers\Database();
+    }
+    catch(\PDOException $e) { // connection failed
+        define('COCKPIT_TABLES_CONNECTED', false);
+    }
+
+    if(!defined('COCKPIT_TABLES_CONNECTED'))
+        define('COCKPIT_TABLES_CONNECTED', true);
+
 });
+
+include_once(__DIR__.'/tables.php');
+
+// ADMIN
+if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
+    include_once(__DIR__.'/admin.php');
+}
