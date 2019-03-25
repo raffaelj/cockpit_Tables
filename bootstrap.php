31011182bb70aa0fa3895a0f8713732bc64a99b4
Raffael <github@raffaeljesche.de>
2019-03-25 19:20:50 +0100

    added snippet to prevent wrong folder name, added version

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,29 @@
 <?php
+/**
+ * SQL table manager for Cockpit CMS
+ * 
+ * @see       https://github.com/raffaelj/cockpit_Tables/
+ * @see       https://github.com/agentejo/cockpit/
+ * 
+ * @version   0.1.0
+ * @author    Raffael Jesche
+ * @license   MIT
+ * @note      work in progress
+ */
+
+// controller autoload and path short codes need a folder naming pattern
+// return if addon folder has wrong name, e. g. "cockpit_AddonName"
+$name = 'Tables';
+
+if (!isset($app['modules'][strtolower($name)])) {
+
+    // display a warning on top of admin ui
+    $app->on('app.layout.contentbefore', function() use ($name) {
+        echo '<p><span class="uk-badge uk-badge-warning"><i class="uk-margin-small-right uk-icon-warning"></i>' . $name . '</span> You have to rename the addon folder <code>' . basename(__DIR__) . '</code> to <code>' . $name . '</code>.</p>';
+    });
+
+    return;
+}
 
 // simple logging for debugging
 function debug($message) {
